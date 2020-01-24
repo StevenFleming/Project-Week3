d1685985845f2b78fa0f80bae48b1e6c9bb4860d
stevenfleming <12flemings@gmail.com>
2020-01-24 11:57:01 -0800

    attempts to find and replace instances of 1 using regular expressions, does not currently work

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
+// Backend Logic
+
+var ifTwo = /[2]/i;
+var ifThree = /[3]/i;
+
+function ReplaceIfOne(inputString){
+  var ifONe = /[1]/g;
+  var beepString = string.replace(ifONe, "Beep");
+  return beepString;
+}
+
+
+var inputString = ($("input#limit").val());
+console.log(inputString)
+var inputArray = inputString.split("");
+console.log(inputArray)
+var inputInterger = parseInt($("input#limit").val());
+console.log(inputInterger)
+
+
+// Frontend logic User interface
 $(document).ready(function() {
   $("form#form1").submit(function(event) {
 
     event.preventDefault();
-
-    var inputString = ($("input#limit").val());
-    console.log(inputString)
-    var inputArray = inputString.split("");
-    console.log(inputArray)
-    var inputInterger = parseInt($("input#limit").val());
-    console.log(inputInterger)
   });
 });
 
