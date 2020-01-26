4580d1833819498cc50adccdee89034bad8009fc
stevenfleming <12flemings@gmail.com>
2020-01-25 19:34:24 -0800

    cleans up more of the code

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,23 @@ $(document).ready(function(){
   $("#form1").submit(function(event){
     event.preventDefault();
     var inputArray = []
-    var inputInt = parseInt($("#userInput").val());
-      for (i= 1; i <= inputInt; i ++) {
+    var inputstr = ($("#userInput").val());
+      for (i= 1; i <= inputstr; i ++) {
         numberstrings= i.toString();
         if (numberstrings.includes("3")) {
-          var Dave = "I'm sorry, Dave. I'm afraid I can't do that. "
+          var Dave = " I'm sorry, Dave. I'm afraid I can't do that. "
           inputArray[i] = Dave;
           }
         else if (numberstrings.includes("2")) {
-            var Boop = "Boop "
+            var Boop = " Boop "
             inputArray[i] = Boop;
             }
         else if (numberstrings.includes("1")) {
-              var Beep = "Beep "
+              var Beep = " Beep "
               inputArray[i] = Beep;
               }
         else { 
               inputArray.push([i]);
-              inputArray.toString(); 
             }
   }
     $("#output").text(`Translation: ${inputArray}`)
