0d6ee3aac44727f4e0cc8d7856765598da8717f2
stevenfleming <12flemings@gmail.com>
2020-01-25 20:39:38 -0800

    removes local variable from each if/else if loop

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,13 @@ $(document).ready(function(){
       for (i= 1; i <= inputstr; i ++) {
         numberstrings= i.toString();
         if (numberstrings.includes("3")) {
-          var Dave = " I'm sorry, Dave. I'm afraid I can't do that. "
-          inputArray[i] = Dave;
+          inputArray[i] = " I'm sorry, Dave. I'm afraid I can't do that. ";
           }
         else if (numberstrings.includes("2")) {
-            var Boop = " Boop "
-            inputArray[i] = Boop;
+            inputArray[i] = " Boop ";
             }
         else if (numberstrings.includes("1")) {
-              var Beep = " Beep "
-              inputArray[i] = Beep;
+              inputArray[i] = " Beep ";
               }
         else { 
               inputArray.push([i]);
