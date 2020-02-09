d55e710787ec0b09da426bb4971398ddb62321d7
Student <example@example.com>
2020-02-08 18:53:02 -0800

    splits backend and frontend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,31 @@
+//Backend Logic
+
+function computerSpeak(numb) {
+  var inputArray = [];
+  for (i= 1; i <= numb; i ++) {
+    numberstrings= i.toString();
+    if (numberstrings.includes("3")) {
+      inputArray[i] = " I'm sorry, Dave. I'm afraid I can't do that. ";
+      }
+    else if (numberstrings.includes("2")) {
+        inputArray[i] = " Boop ";
+        }
+    else if (numberstrings.includes("1")) {
+          inputArray[i] = " Beep ";
+          }
+    else { 
+          inputArray.push([i]);
+        }
+      }
+      return inputArray
+  }
+
+// User Interface Logic
 $(document).ready(function(){
   $("#form1").submit(function(event){
-    event.preventDefault();
-    var inputArray = []
     var inputstr = ($("#userInput").val());
-      for (i= 1; i <= inputstr; i ++) {
-        numberstrings= i.toString();
-        if (numberstrings.includes("3")) {
-          inputArray[i] = " I'm sorry, Dave. I'm afraid I can't do that. ";
-          }
-        else if (numberstrings.includes("2")) {
-            inputArray[i] = " Boop ";
-            }
-        else if (numberstrings.includes("1")) {
-              inputArray[i] = " Beep ";
-              }
-        else { 
-              inputArray.push([i]);
-            }
-          }
-    $("#output").text(`Translation: ${inputArray}`)
+    $("#output").text(computerSpeak(inputstr));
+    event.preventDefault();
   });
 });
 
